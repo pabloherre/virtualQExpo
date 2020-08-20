401a0d2a829216270eb453790b31e3ddabe51159
Pablo A. Herrera <pablo.a.herrera@gmail.com>
2020-08-20 14:55:06 -0300

    remove unused imports

--- src/views/Appointments/AppointmentDetails/AppointmentDetailsView.test.js
+++ src/views/Appointments/AppointmentDetails/AppointmentDetailsView.test.js
@@ -3,8 +3,8 @@ import { connectedRender } from '../../../../jest/test-utils';
 import { appointment } from '../../../../jest/mocks';
 import AppointmentDetailsView from './AppointmentDetailsView';
 
-describe.only('<AppointmentDetailsView />', () => {
-  it.only('has 1 child', async () => {
+describe('<AppointmentDetailsView />', () => {
+  it('has 1 child', async () => {
     const haveChildren = connectedRender(<AppointmentDetailsView appointment={appointment} />, {}).toJSON();
     expect(haveChildren.children.length).toBe(1);
   });
